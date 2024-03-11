f08d545ca0fa609b364c76527c047aad4a8161c5
Ananda Vilar Vidal <115959226+4nandaw@users.noreply.github.com>
2024-03-11 14:11:19 -0300

    P2

---
+++ 2/fibonacci.hs
@@ -0,0 +1,15 @@
+fibonacci:: [Integer]
+fibonacci = 0 : 1 : zipWith (+) fibonacci (tail fibonacci)
+
+soma:: Integer -> Integer
+soma = sum . map (read . return) . show
+
+fib:: Integer -> Integer
+fib x = head $ filter (\a -> soma a == x) fibonacci
+
+main:: IO()
+main = do
+    input <- getLine
+    let somaTarget = read input
+        resultado = fib somaTarget
+    putStrLn(show resultado)
