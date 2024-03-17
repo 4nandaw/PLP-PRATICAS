4d381f440cf127f0d2a54278bcd1c0ca9d2ca125
Ananda Vilar Vidal <ananda.vilar.vidal@ccc.ufcg.edu.br>
2024-03-17 18:03:32 -0300

    P4

--- PraticasFuncionais/4/sublistas.hs
+++ PraticasFuncionais/4/sublistas.hs
@@ -1,13 +1,18 @@
-sublistas:: [Int] -> Int
-sublistas [] = 0
-sublistas [_] = 0
-sublistas lista
-    | (head lista) > (head (tail lista)) = 1 + sublistas (tail lista)
-    | otherwise = 0 + sublistas (tail lista)
+countSublists :: [Int] -> Int
+countSublists [] = 0
+countSublists xs = sublistsAux xs 0 0
+
+sublistsAux :: [Int] -> Int -> Int -> Int
+sublistsAux [] _ count = count
+sublistsAux [x] _ count = count + 1
+sublistsAux (x:y:xs) len count
+    | y > x     = sublistsAux (y:xs) (len + 1) count
+    | len > 0   = sublistsAux (y:xs) 0 (count + 1)
+    | otherwise = sublistsAux (y:xs) 0 count
 
 
 main:: IO()
 main = do
     input <- getLine
     let lista = read input
-    putStrLn(show (sublistas lista))
+    putStrLn(show (countSublists lista))
